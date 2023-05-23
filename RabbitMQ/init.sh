0b469cd22cda5f13d8ef17aa976ecee38e2b8068
Sven <github.mm1gw@simplelogin.co>
2023-05-23 18:35:54 +0200

    Add RabbitMQ + instructies

---
+++ RabbitMQ/init.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+QUEUE_NAME="ball_queue"
+
+# Wacht tot RabbitMQ klaar is om verbindingen te accepteren
+while ! rabbitmqctl status; do sleep 5; done
+
+# Controleer of de queue al bestaat
+EXISTING_QUEUE=$(rabbitmqctl list_queues --quiet --no-table-headers | grep -e "^${QUEUE_NAME} ")
+
+if [ -z "$EXISTING_QUEUE" ]; then
+  # Maak de queue
+  rabbitmqadmin declare queue name=$QUEUE_NAME durable=true
+  echo "Queue '${QUEUE_NAME}' aangemaakt."
+else
+  echo "Queue '${QUEUE_NAME}' bestaat al."
+fi
