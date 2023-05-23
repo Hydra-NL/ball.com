c2845ab2bc7799d0a882a75469060713c043e72d
Sven <67653224+sv3nnie@users.noreply.github.com>
2023-05-23 19:14:42 +0200

    change bash script for multiple queues

--- RabbitMQ/init.sh
+++ RabbitMQ/init.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-QUEUE_NAME="ball_queue"
+QUEUES=("customer" "supplier" "order" "delivery" "servicedepartment")
 
 # Wacht tot RabbitMQ klaar is om verbindingen te accepteren
 while ! rabbitmqctl status; do sleep 5; done
 
-# Controleer of de queue al bestaat
-EXISTING_QUEUE=$(rabbitmqctl list_queues --quiet --no-table-headers | grep -e "^${QUEUE_NAME} ")
-
-if [ -z "$EXISTING_QUEUE" ]; then
-  # Maak de queue
-  rabbitmqadmin declare queue name=$QUEUE_NAME durable=true
-  echo "Queue '${QUEUE_NAME}' aangemaakt."
-else
-  echo "Queue '${QUEUE_NAME}' bestaat al."
-fi
+# Maak queues die nog niet bestaan
+for QUEUE_NAME in "${QUEUES[@]}"
+do
+    EXISTING_QUEUE=$(rabbitmqctl list_queues --quiet --no-table-headers | grep -e "^${QUEUE_NAME} ")
+    if [ -z "$EXISTING_QUEUE" ]; then
+      rabbitmqadmin declare queue name=$QUEUE_NAME durable=true
+      echo "Queue '${QUEUE_NAME}' aangemaakt."
+    else
+      echo "Queue '${QUEUE_NAME}' bestaat al."
+    fi
+done
