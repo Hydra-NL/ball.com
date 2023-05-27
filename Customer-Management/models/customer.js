8764f723fe6f5edbf51b7355aeab8fa299da8d23
Thijs <thijsvanrijsbergen@gmail.com>
2023-05-27 16:55:44 +0200

    customer in compose

--- Customer-Management/models/customer.js
+++ Customer-Management/models/customer.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   "administrator", // username
   "password123", // password
   {
-    host: "localhost", // MySQL container hostname (if running on the same machine as this app, or the IP address of the machine running the MySQL container, if running on separate machines)
+    host: "mysql-standalone", // MySQL container hostname (if running on the same machine as this app, or the IP address of the machine running the MySQL container, if running on separate machines)
     port: 3306,
     dialect: "mysql",
     logging: false,
@@ -92,7 +92,7 @@ const Customer = sequelize.define(
 sequelize
   .sync({ force: false })
   .then(() => {
-    console.log("Order table created successfully!");
+    console.log("Customer table created successfully!");
     // print all tables in the database
     sequelize.query("SHOW TABLES").then((result) => {
       console.log(result[0]);
