89a367e8cb963f3f5bd1e9cdeacd2e1608c5f706
Pascal <p.vandekeere@student.avans.nl>
2023-05-27 19:31:58 +0200

    Supplier docker + rabbitMQ

--- Supplier-Management/app.js
+++ Supplier-Management/app.js
@@ -3,7 +3,9 @@ const supplierRoute = require("./routes/supplier_routes");
 const productRoute = require("./routes/product_routes");
 const bodyParser = require("body-parser");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
+
+const RabbitMQConsumer = require("./rabbitmq/rabbitMQ_consumer");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,6 +22,8 @@ app.use((err, res) => {
 });
 
 app.listen(port, () => {
+  const consumer = new RabbitMQConsumer();
+  consumer.listenToQueue();
   console.log(`Server running at http://localhost:${port}`);
 });
 
