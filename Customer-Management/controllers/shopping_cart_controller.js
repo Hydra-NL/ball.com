0dfcfb3747750d3b6a7586363a0010bbc030ca82
Sven <github.mm1gw@simplelogin.co>
2023-05-30 19:17:45 +0200

    add back merge shopping cart

--- Customer-Management/controllers/shopping_cart_controller.js
+++ Customer-Management/controllers/shopping_cart_controller.js
@@ -205,24 +205,4 @@ module.exports = {
         });
     });
   },
-
-  validateToken(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      return res.status(401).json({ message: "No token provided" });
-    } else {
-      const token = authHeader.substring(7, authHeader.length);
-      jwt.verify(token, config.secret, (err, decoded) => {
-        if (err) {
-          return res.status(401).json({ message: "Invalid token" });
-        } else {
-          // check if token has customerId
-          if (!decoded.sub)
-            return res.status(401).json({ message: "Invalid token" });
-          req.customerId = decoded.sub;
-          next();
-        }
-      });
-    }
-  },
 };
