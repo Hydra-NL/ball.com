30cc6c54474bc29b188919a2816468a8074f7237
Sven <github.mm1gw@simplelogin.co>
2023-05-30 16:27:53 +0200

    add subscription to events

--- Order-Management/eventstore/eventstore_manager.js
+++ Order-Management/eventstore/eventstore_manager.js
@@ -1,19 +1,26 @@
-const { EventStoreDBClient, jsonEvent } = require("@eventstore/db-client");
+const { EventStoreDBClient, jsonEvent, SubscribeToAllOptions } = require("@eventstore/db-client");
 const { eventstoredb: config } = require("../config.json");
 const { FORWARDS } = require("@eventstore/db-client");
+const listenEvents = ["OrderCreated"];
 
 const connectionString = "esdb://eventstoredb?tls=false";
 const client = EventStoreDBClient.connectionString(connectionString);
 
+async function subscribeToStream() {
+    // make subscription to events, only listen to new events coming in after subscription
+    const subscription = client.subscribeToAll();
+
+    for await (const resolvedEvent of subscription) {
+        const event = resolvedEvent.event;
+        if (event.type === "OrderCreated") {
+            console.log("Received event:", event);
+        }
+    }
+}
+
 async function appendToStream(stream, eventName, eventData) {
     const event = jsonEvent({ type: eventName, data: eventData });
     await client.appendToStream(stream, [event]);
-
-    const events = await getSpecificEvents(stream, 'OrderCreated');
-    // loop over events
-    for (const event of events) {
-        console.log(event);
-    }
 }
 
 async function readFromStream(stream) {
@@ -34,6 +41,8 @@ async function getSpecificEvents(stream, eventName) {
 
     return specificEvents;
 }
+
+subscribeToStream();
 
 module.exports = {
     appendToStream,
